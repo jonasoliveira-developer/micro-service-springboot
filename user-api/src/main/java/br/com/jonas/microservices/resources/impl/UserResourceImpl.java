4acb48876b9ee7493cd44a0a553263fe76e6ef63
RecantoDeItapua <jonasoliveira.webdeveloper@gmail.com>
2023-05-30 15:36:15 -0300

    created doker images

---
+++ user-api/src/main/java/br/com/jonas/microservices/resources/impl/UserResourceImpl.java
@@ -0,0 +1,31 @@
+package br.com.jonas.microservices.resources.impl;
+
+import br.com.jonas.microservices.domain.User;
+import br.com.jonas.microservices.resources.UserResource;
+import br.com.jonas.microservices.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/users")
+public class UserResourceImpl implements UserResource {
+
+    private final UserService service;
+
+    @Override
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @Override
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+}
